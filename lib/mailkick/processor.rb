f2b2c26bc52c893dbb0bd8d0101a01b848172f28
Nicolas Leger <nicolas.leger@nleger.com>
2016-06-15 08:24:28 +0200

    Fix token rewriting

--- lib/mailkick/processor.rb
+++ lib/mailkick/processor.rb
@@ -21,7 +21,7 @@ module Mailkick
       parts = message.parts.any? ? message.parts : [message]
       parts.each do |part|
         # Avoid Frozen String change and private update method
-        part.body.send(:raw_source, part.body.raw_source.gsub(/%7B%7BMAILKICK_TOKEN%7D%7D/, CGI.escape(token)))
+        part.body.send(:raw_source=, part.body.raw_source.gsub(/%7B%7BMAILKICK_TOKEN%7D%7D/, CGI.escape(token)))
       end
     end
   end
